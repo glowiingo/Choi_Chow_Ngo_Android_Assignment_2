39cd3c780a1f904ff4f75db9ba1ffa5f400e4753
glowiingo <gngo2@my.bcit.ca>
2020-03-02 11:15:33 -0800

    adding class and ui changes

--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference dbref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbref = FirebaseDatabase.getInstance().getReference().child("");
+
     }
 }
---
+++ app/src/main/java/com/example/assignment_2/User.java
@@ -0,0 +1,41 @@
+package com.example.assignment_2;
+
+public class User {
+    private String name;
+    private Float systolic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getSystolic() {
+        return systolic;
+    }
+
+    public void setSystolic(Float systolic) {
+        this.systolic = systolic;
+    }
+
+    public Float getDiastolic() {
+        return diastolic;
+    }
+
+    public void setDiastolic(Float diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    private Float diastolic;
+    private String condition;
+}
