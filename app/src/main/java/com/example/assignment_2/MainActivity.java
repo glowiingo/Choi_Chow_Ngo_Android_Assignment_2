215712ace2a8d642fddbaa53e9818e4aee87c646
glowiingo <gngo2@my.bcit.ca>
2020-03-02 12:13:29 -0800

    added button to main

--- app/src/main/java/com/example/assignment_2/MainActivity.java
+++ app/src/main/java/com/example/assignment_2/MainActivity.java
@@ -3,18 +3,30 @@ package com.example.assignment_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Define variables
     DatabaseReference dbref;
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbref = FirebaseDatabase.getInstance().getReference().child("");
+
+        EditText name_edit_txt = (EditText) findViewById(R.id.name);
+        EditText systolic_input = (EditText) findViewById(R.id.systolic_reading);
+        EditText diastolic_input = (EditText) findViewById(R.id.diastolic_reading);
+        Button saveButton = (Button) findViewById(R.id.save_button);
+
+        dbref = FirebaseDatabase.getInstance().getReference().child("User");
+        user = new User();
 
     }
 }
