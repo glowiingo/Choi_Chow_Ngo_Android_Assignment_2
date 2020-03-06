3a5c45f84acc173d83df47c2165b3ca86c2601a8
glowiingo <gngo2@my.bcit.ca>
2020-03-05 17:08:19 -0800

    update

--- app/src/main/java/com/example/assignment_2/DeleteActivity.java
+++ app/src/main/java/com/example/assignment_2/DeleteActivity.java
@@ -3,7 +3,6 @@ package com.example.assignment_2;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
