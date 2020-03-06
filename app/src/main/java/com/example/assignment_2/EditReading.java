4d11877c4649c61c78b0a5c20dea60ff1a812406
glowiingo <gngo2@my.bcit.ca>
2020-03-06 11:42:00 -0800

    added edit reading activity

---
+++ app/src/main/java/com/example/assignment_2/EditReading.java
@@ -0,0 +1,14 @@
+package com.example.assignment_2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditReading extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_reading);
+    }
+}
